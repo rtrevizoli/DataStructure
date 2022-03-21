9384e071555741497f2ac780d7994142e4535804
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2022-03-21 20:22:07 -0300

    functions agregated
    separate it in last commit

---
+++ LinkedList.c
@@ -0,0 +1,84 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+
+typedef struct List 
+{ 
+    int value; 
+    struct List* next; 
+} List; 
+
+List* insert(int value, List* cel, int pos)  
+{ 
+    List* newCel; 
+
+    newCel = malloc(sizeof(List)); 
+    
+    newCel->value = value; 
+    
+    if (pos == 0) {
+        newCel->next = cel;
+    } else if (pos == -1) {
+        List *p = cel;
+        List *q = cel->next;
+        
+        while (q != NULL) {
+            p = q;
+            q = q->next;
+        }
+        
+        p->next = newCel;
+        newCel->next = NULL;
+
+        return cel;
+    } else {
+        List *p = cel;
+        List *q = cel->next;
+        int i = 1;
+        
+        while (q != NULL) {
+            p = q;
+            q = q->next;
+            
+            i++;
+            
+            if (i == pos)
+                break;
+                
+        }
+        
+        p->next = newCel;
+        newCel->next = q;
+        
+        return cel;
+    }
+    
+    return newCel;
+} 
+
+void print(List* lst) { 
+    List *p;
+    for(p = lst; p != NULL; p = p->next)
+        printf("%d\n", p->value);
+} 
+
+int main() 
+{ 
+    List* lst; 
+ 
+    /*lst = malloc(sizeof(List));
+    lst->next=NULL;*/
+    
+    lst = insert(10, lst, 0); 
+    lst = insert(20, lst, 0); 
+    lst = insert(30, lst, -1);
+    lst = insert(40, lst, -1);
+    lst = insert(50, lst, 0);
+    lst = insert(60, lst, 2);
+    
+    /*50 - 20 - 10 - 30 - 40*/
+    
+    print(lst);
+
+    return 0;
+} 
