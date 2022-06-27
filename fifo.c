a9eeb8f4699d6a391fc733108b3a7f5c310cccf8
rafael.trevizoli <rafael.trevizoli@autaza.com>
2022-06-26 21:33:28 -0300

    Insert elements on Queue

--- fifo.c
+++ fifo.c
@@ -4,10 +4,41 @@
 typedef struct Cel 
 { 
     int value; 
-    struct Cel* next; 
+    struct Cel *next; 
 } Cel; 
+
+void printList(Cel *lst) 
+{ 
+    Cel *p;
+    for(p = lst; p != NULL; p = p->next)
+        printf("%d->", p->value);
+} 
+
+void insertQueue(int value, Cel **iniCel, Cel **finiCel)
+{
+    Cel *newCel;
+    
+    newCel = malloc(sizeof(Cel)); 
+    newCel->value = value;
+    newCel->next = NULL;
+
+    if (*finiCel == NULL) *finiCel = *iniCel = newCel;
+    else
+    {
+        (*finiCel)->next = newCel;
+        *finiCel = newCel;
+    }
+}
 
 int main() 
 {
+    Cel *pi = NULL, *pf = NULL;
+
+    insertQueue(10, &pi, &pf);
+    insertQueue(20, &pi, &pf);
+    insertQueue(30, &pi, &pf);
+
+    printList(pi);
+
     return 0;
 }
